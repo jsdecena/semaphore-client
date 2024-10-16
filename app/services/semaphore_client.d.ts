dfada0ef35b3dd03002bcae1c8a066ffbcd6c33b
Jeff Simons Decena <jeff.decena@yahoo.com>
2024-10-17 00:06:51 +1300

    :tada: v0.0.8

--- app/services/semaphore_client.d.ts
+++ app/services/semaphore_client.d.ts
@@ -1,4 +1,4 @@
-declare module 'semaphore_client' {
+declare module 'semaphore-client' {
   export function lookUpAccount(): Promise<any>
   export function lookUpSms(): Promise<any>
   export function sendSms(): Promise<any>
