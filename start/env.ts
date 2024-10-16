b7bc84bdcb233abf55773d0dddd418972478f7db
Jeff Simons Decena <jeff.decena@yahoo.com>
2024-10-16 22:28:22 +1300

    :tada: Add the env

--- start/env.ts
+++ start/env.ts
@@ -17,4 +17,6 @@ export default await Env.create(new URL('../', import.meta.url), {
   APP_KEY: Env.schema.string(),
   HOST: Env.schema.string({ format: 'host' }),
   LOG_LEVEL: Env.schema.string(),
+  SEMAPHORE_API_KEY: Env.schema.string(),
+  SEMAPHORE_URL: Env.schema.string(),
 })
