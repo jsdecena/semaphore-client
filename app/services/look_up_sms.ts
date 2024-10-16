01756294cc6d08412878bf9c1a408667188c1a6e
Jeff Simons Decena <jeff.decena@yahoo.com>
2024-10-16 23:11:37 +1300

    :tada: update README

--- app/services/look_up_sms.ts
+++ app/services/look_up_sms.ts
@@ -2,14 +2,14 @@ import env from '#start/env'
 import axios from 'axios';
 import { API_KEY } from './index.js';
 
-export const lookUpAccount = async() => {
+export const lookUpSms = async() => {
   
   const BASE_URL = env.get('SEMAPHORE_URL')
   if(!BASE_URL && !API_KEY) {
     throw Error('You must have SEMAPHORE_URL and API_KEY in your env')
   }
 
-  axios.get(`${BASE_URL ?? ''}/account?apikey=${API_KEY}`)
+  axios.get(`${BASE_URL ?? ''}/messages?apikey=${API_KEY}`)
   .then(response => {
     console.log('Account Info:', response.data);
   })
